36496f54d91f58c48ceb6127f90cc06119db441c
ShiekhAmish <77582706+ShiekhAmish@users.noreply.github.com>
2022-04-12 19:45:07 +0500

    part 04 (Use of Refactored Code)
    part 04 (Use of Refactored Code)

--- BMI_AMISH_090/lib/input.dart
+++ BMI_AMISH_090/lib/input.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Input extends StatefulWidget {
   const Input({Key? key}) : super(key: key);
@@ -15,7 +15,7 @@ class _InputState extends State<Input> {
 
       appBar: AppBar(
 
-        title: Text("BMI Calculator"),
+        title: Text(" BMI Calculator"),
       ),
           body: Column(
         children: [
@@ -23,9 +23,17 @@ class _InputState extends State<Input> {
             children:  [
               Expanded(child: RepeatContainer(
                 colors: Colors.red,
+                cardWidget: RepeatIcon(
+                  icon: FontAwesomeIcons.male,
+                  label: 'MALE',
+                ),
               ),),
               Expanded(child: RepeatContainer(
                 colors: Colors.amber,
+                cardWidget: RepeatIcon(
+                  icon: FontAwesomeIcons.female,
+                  label: 'FEMALE',
+                ),
               ),),
             ],
           ),),
@@ -33,6 +41,7 @@ class _InputState extends State<Input> {
             children: [
               Expanded(child: RepeatContainer(
                 colors: Colors.green,
+                cardWidget: Container(),
               ),),
             ],
           ),),
@@ -40,9 +49,11 @@ class _InputState extends State<Input> {
             children: [
               Expanded(child: RepeatContainer(
                 colors: Colors.blueAccent,
+                cardWidget: Container(),
               ),),
               Expanded(child: RepeatContainer(
                 colors: Colors.purple,
+                cardWidget: Container(),
               ),),
             ],
           ),),
@@ -53,14 +64,43 @@ class _InputState extends State<Input> {
   }
 }
 
-class RepeatContainer extends StatelessWidget {
-  RepeatContainer({required this.colors});
-  final Color colors;
+class RepeatIcon extends StatelessWidget {
+RepeatIcon({required this.icon, required this.label});
+  final IconData icon;
+  final String label;
+
 
   @override
   Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+        children:  [
+         Icon(
+            icon,
+            size: 80.0,
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          Text(
+            label,
+            style: const TextStyle(fontSize: 18.0,
+              color: Colors.white),),
+        ],
+    );
+  }
+}
+
+class RepeatContainer extends StatelessWidget {
+
+   RepeatContainer({required this.colors, required this.cardWidget});
+   final Color colors;
+   final Widget cardWidget;
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(15.0),
+      child: cardWidget,
+      margin: const EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colors,
         borderRadius: BorderRadius.circular(10.0),
--- BMI_AMISH_090/lib/main.dart
+++ BMI_AMISH_090/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'input.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 void main() {
   runApp(const BMI());
 }
