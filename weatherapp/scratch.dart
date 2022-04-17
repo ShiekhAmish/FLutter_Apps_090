c1fccfe4be0378a873d330b53b727fa65c140753
ShiekhAmish <77582706+ShiekhAmish@users.noreply.github.com>
2022-04-17 15:45:33 +0500

    Part 03(Async Await and Future Keyword)
    Part 03(Async Await and Future Keyword)

---
+++ weatherapp/scratch.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+void main()
+{
+  performTasks();
+}
+Future<void> performTasks()
+async {
+  task1();
+  String r= await task2();
+  task3(r);
+}
+void task1()
+{
+  String result = 'task 1 data';
+  print("Task 1 Complete");
+}
+Future<String> task2() async {
+  Duration d = Duration(seconds: 5);
+  String result='2';
+  await Future.delayed(d,(){
+   result = 'task 2 data';
+    print("Task 2 Complete");
+  });
+  return result;
+}
+void task3(resultof2)
+{
+  String result = 'task 3 data';
+  print("Task 3 Complete $resultof2");
+}
